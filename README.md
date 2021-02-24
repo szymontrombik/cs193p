39a2b96ebe8e42f46268afa7c8a8d957ff3af392
Szymon Trombik <szymon.trombik@sienn.com>
2021-02-24 21:07:34 +0100

    [lesson1] added cardView + basic game view

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cs193p
+
+## lesson #1
+### overview
+IDE overview + create basic cardView to show main view with four emoji cards.
+### reference
+video: [link to yt movie](https://youtu.be/jbtqIBpUG7g)
+### results
+![Screenshot with cards faced up](./images/cardGame0101_faceUp.png =250x)
+![Screenshot with cards faced down](./images/cardGame0102_faceDown.png =250x)
--- cardGame/cardGame/ContentView.swift
+++ cardGame/cardGame/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach ( 0..<4 ) { index in
+                CardView()
+            }
+        }
+        .padding()
+        .foregroundColor(Color.blue)
+        .font(.title)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
+        if(isFaceUp) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 5)
+                RoundedRectangle(cornerRadius: 10.0).foregroundColor(Color.white)
+                Text("🍕")
+            }
+        } else
+        {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+                Text("?").foregroundColor(Color.white)
+            }
+        }
     }
 }
 
