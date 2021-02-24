3778d442b80f9bf22382cbd4e9dbc2148465d14c
Szymon Trombik <szymon.trombik@sienn.com>
2021-02-24 20:27:37 +0100

    [lesson1] init

---
+++ cardGame/cardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cardGame
+//
+//  Created by MacOS on 24/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cardGame/cardGame/cardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardGameApp.swift
+//  cardGame
+//
+//  Created by MacOS on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct cardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
